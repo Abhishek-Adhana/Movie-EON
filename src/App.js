83bf5475aa33b30f4f0272f01ea12921cd0da9c3
Abhishek-Adhana <abhishekadhana001@gmail.com>
2024-09-23 14:01:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import './App.css';
+import Header from './components/Header/Header';
+import { Container } from '@mui/material';
+import SimpleBottomNavigation from './components/Footer/MainNav';
+import Trending from './Pages/Trending/Trending';
+import Movies from './Pages/Movies/Movies';
+import Series from './Pages/Series/Series';
+import Search from './Pages/Search/Search';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+    <div className="app">
+    <Container>
+    <Routes>
+    <Route path = '/' Component={Trending}/>
+    <Route path = '/movies' Component={Movies}/>
+    <Route path = '/series' Component={Series}/>
+    <Route path = '/search' Component={Search}/>
+    </Routes>
+    </Container>
+    </div>
+    <SimpleBottomNavigation/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
